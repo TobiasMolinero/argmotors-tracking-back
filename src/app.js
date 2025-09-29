6e6b286ebbba3031be9e321319060ae1183f3073
Tobias Molinero <tobiasmolinero98@gmail.com>
2025-09-29 17:07:24 -0300

    change: irigin cors request

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(compression())
 app.use(morgan('dev'));
 app.use(cors({
-    origin: "https://argmotors-tracking-front.vercel.app/",
+    origin: "https://argmotors-tracking-front.vercel.app",
     methods: ["GET", "POST", "PATCH"]
 }));
 
